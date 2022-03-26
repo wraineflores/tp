7257949b8a795f957ea5d2201e960c1c388d3696
wraineflores <wraineflores@gmail.com>
2022-03-26 12:28:29 +0800

    Added last line in file

--- src/test/java/seedu/duke/helper/CommandTest.java
+++ src/test/java/seedu/duke/helper/CommandTest.java
@@ -1,14 +1,9 @@
 package seedu.duke.helper;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 
 import java.util.logging.Logger;
-
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class CommandTest {
